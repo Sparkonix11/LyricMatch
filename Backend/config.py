bfc76a85fefe4ef45dfe019a910812eb577ba087
Abhishek <abhishekbidhan11@gmail.com>
2025-03-22 04:42:09 +0530

    added postgress db

--- Backend/config.py
+++ Backend/config.py
@@ -1,5 +1,5 @@
 SECRET_KEY = 'secretkey'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://lyric_match_user:IgF66rfGvrJmQ1xNSI70oUJqwsnGe6eF@dpg-cveutl0fnakc739ht9gg-a.singapore-postgres.render.com/lyric_match'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
 TESTING = False
