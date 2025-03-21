60538b6775d67bcbaa713aefccdb13bca476ca5e
Abhishek <abhishekbidhan11@gmail.com>
2025-03-22 05:14:24 +0530

    hardcode remote address

--- Frontend/src/services/apis.js
+++ Frontend/src/services/apis.js
@@ -1,4 +1,6 @@
-const API_URL = `${import.meta.env.VITE_SERVER_URL}/api/v1`;
+// const API_URL = `${import.meta.env.VITE_SERVER_URL}/api/v1`;
+const API_URL = `https://lyricmatch-gm7t.onrender.com/api/v1`;
+
 
 export const songEndpoints = {
     GENERATE_LYRICS: `${API_URL}/random-lyric`,
